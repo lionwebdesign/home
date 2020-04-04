2511e988c287a563e330001948444922b8661515
Ruben V <vim.ruben@hotmail.com>
2020-04-04 15:26:37 -0300

    Primera visualizacion

---
+++ js/js.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var scroll = $(window).scrollTop();
+    $('#posicion').text(scroll);
+    $(window).scroll(function(event) {
+        var scroll = $(window).scrollTop();
+        $('#posicion').text(scroll);
+        /*Cambia el color del div cuando es distinto a 0*/
+        if(scroll!=0){
+            $('#nav-dinamico').css({
+                background: '#262626'
+            });
+        }else{
+            $('#nav-dinamico').css({
+                background: 'transparent'
+            });
+        }
+    });
+    });
